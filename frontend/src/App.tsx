aa87ea5444ed3456578cb2ab590a1625c657c397
Anthony M <anthony.moffa@thoughtbot.com>
2021-12-18 19:40:47 -0500

    Hide main div when account is undefined

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,22 @@ import React from "react";
 import Header from "./Header";
 import VaultDetails from "./VaultDetails";
 import VaultControls from "./VaultControls";
+import { useEthers } from "@usedapp/core";
 
 const App = () => {
+  const { account } = useEthers();
+
   return (
     <div className="bg-gradient-to-b from-red-200 to-slate-800 h-full">
       <Header />
-      <div className="flex flex-col w-full max-w-md mx-auto my-0 pt-5">
-        <div className="border border-slate-800 rounded-lg py-5 bg-slate-800 filter drop-shadow-lg text-white">
-          <VaultDetails />
-          <VaultControls />
+      {account && (
+        <div className="flex flex-col w-full max-w-md mx-auto my-0 pt-5">
+          <div className="border border-slate-800 rounded-lg py-5 bg-slate-800 filter drop-shadow-lg text-white">
+            <VaultDetails />
+            <VaultControls />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
