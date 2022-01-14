b9b28b0075e313a911e3b5f439ae132fa2637629
Anthony M <anthony.moffa@thoughtbot.com>
2022-01-13 20:44:55 -0500

    Move address to header button

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,25 +1,14 @@
 import React from "react";
-import { useEthers } from "@usedapp/core";
+import WalletConnector from "./WalletConnector";
 
 const Header = () => {
-  const { account, activateBrowserWallet, deactivate } = useEthers();
-
-  const button = account
-    ? { label: "Disconnect", onClick: deactivate }
-    : {
-        label: "Connect Wallet",
-        onClick: activateBrowserWallet,
-      };
-
   return (
     <div className="flex justify-between items-center py-5 px-10">
-      <div className="w-24" />
+      <div className="w-52" />
       <div className="text-3xl font-semibold justify-start self-center space-x-10">
         Diamond Hands Only
       </div>
-      <div className="text-xl self-center px-5 py-3 border border-gray-400 rounded-full">
-        <button onClick={() => button.onClick()}>{button.label}</button>
-      </div>
+      <WalletConnector />
     </div>
   );
 };
